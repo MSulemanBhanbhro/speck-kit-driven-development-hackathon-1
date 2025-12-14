d1a8f57d1155e92af12f8ab50b1a86515f31bd26
M Suleman <157698147+MSulemanBhanbhro@users.noreply.github.com>
2025-12-14 20:19:16 +0500

    updated issues

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   // Set the /<base>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<organization-name>/'
   baseUrl: '/',
-  trailingSlash: true,
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   organizationName: 'ai-robotics-book', // Usually your GitHub org/user name.
